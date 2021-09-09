b28ec8b50f1f693c9cd54a5fa070120aeb150723
Cameron <camcreel@gmail.com>
2021-09-09 17:50:29 -0400

    initial user resource creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding"
+User.destroy_all
+User.create({username: "bobcat",password_digest:"abcde"})
+User.create({username:"monk",password_digest: "1234"})
+
+puts "Done seeding"
