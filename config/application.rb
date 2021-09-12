49bbe569332646820aa7fb2f07a928360dc87bf5
Cameron <camcreel@gmail.com>
2021-09-12 17:08:09 -0400

    configuration

--- config/application.rb
+++ config/application.rb
@@ -37,8 +37,8 @@ module JobTrackerBackend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::Session::CookieStore, secure: true
     
-    config.action_dispatch.cookies_same_site_protection = :lax
+    config.action_dispatch.cookies_same_site_protection = :none
   end
 end
