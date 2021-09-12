556b75491925a05ff748776fca830a3ac861df8c
Cameron <camcreel@gmail.com>
2021-09-12 14:33:15 -0400

    reconfigure cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,10 @@ class ApplicationController < ActionController::API
     include ActionController::Cookies
     
     before_action :authorize
-    
+    #skip_before_action :verify_authenticity_token
     private
 
     def authorize
-        
         @current_user = User.find_by(id: session[:user_id])
         if !@current_user
             render json: { errors: ["Not authorized"] }, status: :unauthorized
