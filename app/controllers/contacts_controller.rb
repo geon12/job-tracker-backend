c2cce4f161e89a1d94d5ea3d9b4ca67b1a366cc4
Cameron <camcreel@gmail.com>
2021-09-17 20:02:33 -0400

    add update action to contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,18 @@
 class ContactsController < ApplicationController
+    def update
+        contact = find_contact
+        contact.update(contact_params)
+        render json: contact
+    end
+
+    private
+
+    def find_contact
+        Contact.find(params[:id])
+    end
+
+    def user_params
+        contact.permit(:name,:role,:phone_number,:email,:description)
+    end
+end
 end
