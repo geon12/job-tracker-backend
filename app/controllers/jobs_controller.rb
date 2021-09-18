9cbf6c64e55a7661cbb73ce2e80a028594397b10
Cameron <camcreel@gmail.com>
2021-09-18 18:56:46 -0400

    add actions to jobs controller and organizations controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,18 @@
 class JobsController < ApplicationController
+    def show
+        jobs = Job.all
+        render json: jobs
+    end
+
+    def create
+        job = Job.create!(job_params)
+        render json: job, status: :created
+    end
+
+    private
+
+    def job_params
+        params.permit(:role,:description,:listing_url,:location,:organization_id)
+    end
 end
+
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,2 +1,17 @@
 class OrganizationsController < ApplicationController
+    def show
+        organizations = Organization.all
+        render json: organizations
+    end
+
+    def create
+        organization = Organization.create!(organization_params)
+        render json: organization, status: :created
+    end
+    
+    private
+
+    def organization_params
+        params.permit(:name,:description,:industry)
+    end
 end
